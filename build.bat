e5c1cff939a656352a6c46784541370e03478c2e
Yannick <yannick.valy@gmail.com>
2021-01-05 18:57:26 +0100

    Add build script

---
+++ build.bat
@@ -0,0 +1,2 @@
+cd src
+make all -j4
--- setup-environment.bat
+++ setup-environment.bat
@@ -1,8 +1,9 @@
-echo "get-started.bat"
+kecho "setup-environment.bat"
 echo %userprofile%
 
 cd %userprofile%\esp
 git clone --recursive https://github.com/espressif/esp-adf.git
+git checkout v2.2b
 
 set IDF_PATH=%userprofile%\esp\esp-adf\esp-idf
 set ADF_PATH=%userprofile%\esp\esp-adf
@@ -10,4 +11,3 @@ set ADF_PATH=%userprofile%\esp\esp-adf
 echo %IDF_PATH%
 echo %ADF_PATH%
 
-%userprofile%\esp\esp-adf\esp-idf\export.bat
